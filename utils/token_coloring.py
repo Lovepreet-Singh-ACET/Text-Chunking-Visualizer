156b808af725f8cff6e42704d421f396593e10c9
Lovepreet-Singh-ACET <123lovepreets@gmail.com>
2024-08-29 00:24:31 +0530

    changed the token color to bg

--- utils/token_coloring.py
+++ utils/token_coloring.py
@@ -7,11 +7,12 @@ def color_tokens_with_tiktoken(text, model_name):
     tokens = encoder.encode(text)  # Tokenize text using tiktoken
     decoded_tokens = encoder.decode_tokens_bytes(tokens)  # Decode tokens to their string representation
 
-    colors = ['#FF6666', '#FFCC66', '#99FF66', '#66FFCC', '#66CCFF', '#CC99FF']
+    # colors = ['#FF6666', '#FFCC66', '#99FF66', '#66FFCC', '#66CCFF', '#CC99FF']
+    colors = ['#6B40D84D', '#68DE7A66', '#F4AC3666', '#EF414666', '#27B5EA66']
     colored_tokens = []
-    for token in decoded_tokens:
-        color = random.choice(colors)
-        colored_tokens.append(f"<span style='color: {color};'>{token.decode()}</span>")
+    for idx, token in enumerate(decoded_tokens):
+        color = colors[idx%5]
+        colored_tokens.append(f"<span style='background-color: {color};color: #D9D9E3'>{token.decode()}</span>")
     
     return ' '.join(colored_tokens)
 
@@ -21,11 +22,11 @@ def color_tokens_with_sentence_transformer_tokenizer(text, model_name):
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokens = tokenizer.tokenize(text)
-    colors = ['#FF6666', '#FFCC66', '#99FF66', '#66FFCC', '#66CCFF', '#CC99FF']
-
+    # colors = ['#FF6666', '#FFCC66', '#99FF66', '#66FFCC', '#66CCFF', '#CC99FF']
+    colors = ['#6B40D84D', '#68DE7A66', '#F4AC3666', '#EF414666', '#27B5EA66']
     colored_tokens = []
-    for token in tokens:
-        color = random.choice(colors)
-        colored_tokens.append(f"<span style='color: {color};'>{token}</span>")
+    for idx, token in enumerate(tokens):
+        color = colors[idx%5]
+        colored_tokens.append(f"<span style='background-color: {color};color: #D9D9E3'>{token}</span>")
     
     return ' '.join(colored_tokens)
